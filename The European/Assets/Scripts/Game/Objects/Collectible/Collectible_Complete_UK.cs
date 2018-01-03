adac2ae57b9bc440fa708e918c67c9508e2fc065
Joseph Adams <WASDMagician@gmail.com>
2018-01-03 22:32:44 +0000

    Revert "Revert "Merge branch 'master' of https://github.com/WASDMagician/The-European""
    This reverts commit e66ecdfd6d22a0ce4d07d00d7eae6ebef19f8174.

---
+++ The European/Assets/Scripts/Game/Objects/Collectible/Collectible_Complete_UK.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible_Complete_UK : Collectible_Complete_Trigger {
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public override void Trigger_Completion()
+    {
+        base.Trigger_Completion();
+        print("TRIGGER");
+        if(animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        if(animator != null)
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+}
