749edbaac5bedf9563b7a41d5233fd5aa5fbf02c
Joseph Adams <WASDMagician@gmail.com>
2018-01-03 13:00:04 +0000

    hdtnhd

---
+++ The European/Assets/Scripts/Game/Objects/Collectible/Display_Collectible_Info.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display_Collectible_Info : Interactible {
+
+    public Sprite display_sprite;
+
+    public override void Interact(Character_State _interactor)
+    {
+        base.Interact(_interactor);
+        if(Display_Image.display_image != null)
+        {
+            Display_Image.display_image.Display(display_sprite);
+        }
+    }
+
+    public override void Stop_Interacting()
+    {
+        base.Stop_Interacting();
+        if(Display_Image.display_image != null)
+        {
+            Display_Image.display_image.Hide();
+        }
+    }
+
+}
